cbe12a8ce5d985d0c9eada7a56dec2e89575762b
tony <antonina.gerasiova@gmail.com>
2016-05-09 21:07:05 +0300

    show test image fetched srom the internet

--- app/src/main/java/com/example/android/finalproject/MainActivityFragment.java
+++ app/src/main/java/com/example/android/finalproject/MainActivityFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,10 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+        ImageView testView = (ImageView) rootView.findViewById(R.id.test_view);
+        Picasso.with(getActivity().getApplicationContext()).load("http://image.tmdb.org/t/p/w92/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg").into(testView);
+        return rootView;
     }
 }
