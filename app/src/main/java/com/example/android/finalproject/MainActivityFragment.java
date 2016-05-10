d4b51d976965951be640c0c57df3a8240ede12d2
tony <antonina.gerasiova@gmail.com>
2016-05-10 16:49:55 +0300

    get JSON sring from

--- app/src/main/java/com/example/android/finalproject/MainActivityFragment.java
+++ app/src/main/java/com/example/android/finalproject/MainActivityFragment.java
@@ -1,13 +1,23 @@
 package com.example.android.finalproject;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -24,6 +34,84 @@ public class MainActivityFragment extends Fragment {
         View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
         ImageView testView = (ImageView) rootView.findViewById(R.id.test_view);
         Picasso.with(getActivity().getApplicationContext()).load("http://image.tmdb.org/t/p/w92/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg").into(testView);
+        new FetchMoviesTask().execute();
         return rootView;
+    }
+
+    public class FetchMoviesTask extends AsyncTask<Void, Void, String[]> {
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+             // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String resultJsonStr = null;
+
+            try {
+                 // Construct the URL for  themoviedb.org query
+
+                final String BASE_URL = "http://api.themoviedb.org/3/movie/popular?";
+                final String APIKEY_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                        .appendQueryParameter(APIKEY_PARAM,  BuildConfig.THEMOVIEDB_API_KEY)
+                        .build();
+
+                Log.v(LOG_TAG, "builtUri: " + builtUri);
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to themoviedb.org, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                 // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+
+                resultJsonStr = buffer.toString();
+                Log.v(LOG_TAG, "Result JSON String: " + resultJsonStr);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the movies data, there's no point in attempting
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+
+
+            return new String[0];
+        }
     }
 }
