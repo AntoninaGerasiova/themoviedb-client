c14f8c674dc3fc61528206463f3b3ea400f9a2bf
tony <antonina.gerasiova@gmail.com>
2016-07-11 16:27:12 +0300

    Add strings for movie table and its fields to MovieContract

---
+++ app/src/main/java/com/example/android/finalproject/data/MovieContract.java
@@ -0,0 +1,33 @@
+package com.example.android.finalproject.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Defines table and column names for the movie database.
+ */
+public class MovieContract {
+    /* Inner class that defines the table contents of the movie table */
+
+    public static final class MovieEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "movie";
+
+        // Column with the id of the movie as is in JSON
+        public static final String MOVIE_ID = "movie_id";
+        //title of the movie
+        public static final String TITLE = "title";
+        //partial address of the poster
+        public static final String POSTER = "poster";
+        //partial address of the backdrop
+        public static final String BACKDROP = "backdrop";
+        //overview of the movie
+        public static final String OVERVIEW = "overview";
+        //average vote of the movie
+        public static final String VOTE = "vote";
+        //release date of the movie
+        public static final String RELEASE_DATE = "release_date";
+        //popularity of the movie
+        public static final String POPULARITY = "popularity";
+
+    }
+}
