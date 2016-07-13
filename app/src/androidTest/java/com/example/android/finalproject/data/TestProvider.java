55250eba182f0616f8c0c33b2c26dc9d26a733f5
tony <antonina.gerasiova@gmail.com>
2016-07-13 15:29:06 +0300

    implement insert function in MovieProvider. Add test for it.

--- app/src/androidTest/java/com/example/android/finalproject/data/TestProvider.java
+++ app/src/androidTest/java/com/example/android/finalproject/data/TestProvider.java
@@ -1,11 +1,13 @@
 package com.example.android.finalproject.data;
 
 import android.content.ComponentName;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Build;
 import android.test.AndroidTestCase;
 
@@ -113,6 +115,49 @@ public class TestProvider  extends AndroidTestCase {
             assertEquals("Error: Movie Query did not properly set NotificationUri",
                     movieCursor.getNotificationUri(), MovieEntry.CONTENT_URI);
         }
+
+    }
+
+    /*
+        This test uses the provider to insert and then update the data. Uncomment this test to
+        see if your update movie is functioning correctly.
+     */
+    public void testUpdateMovie() {
+
+    }
+
+    public void testInsertReadProvider() {
+        ContentValues testValues = TestUtilities.createMovieValues();
+
+        // Register a content observer for our insert.  This time, directly with the content resolver
+        TestUtilities.TestContentObserver tco = TestUtilities.getTestContentObserver();
+        mContext.getContentResolver().registerContentObserver(MovieEntry.CONTENT_URI, true, tco);
+        Uri locationUri = mContext.getContentResolver().insert(MovieEntry.CONTENT_URI, testValues);
+
+        // Did our content observer get called?  If this fails, your insert movie
+        // isn't calling getContext().getContentResolver().notifyChange(uri, null);
+        tco.waitForNotificationOrFail();
+        mContext.getContentResolver().unregisterContentObserver(tco);
+
+        long locationRowId = ContentUris.parseId(locationUri);
+
+                // Verify we got a row back.
+        assertTrue(locationRowId != -1);
+
+        // Data's inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
+        // the round trip.
+
+                // A cursor is your primary interface to the query results.
+        Cursor cursor = mContext.getContentResolver().query(
+                MovieEntry.CONTENT_URI,
+                null, // leaving "columns" null just returns all the columns.
+                null, // cols for "where" clause
+                null, // values for "where" clause
+                null  // sort order
+        );
+
+        TestUtilities.validateCursor("testInsertReadProvider. Error validating MovieEntry.",
+                cursor, testValues);
 
     }
 
--- app/src/main/java/com/example/android/finalproject/data/MovieProvider.java
+++ app/src/main/java/com/example/android/finalproject/data/MovieProvider.java
@@ -1,9 +1,11 @@
 package com.example.android.finalproject.data;
 
+import android.annotation.TargetApi;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -28,7 +30,6 @@ public class MovieProvider  extends ContentProvider {
 
         return matcher;
     }
-
 
 
     @Override
@@ -81,10 +82,27 @@ public class MovieProvider  extends ContentProvider {
         }
     }
 
-    @Nullable
+
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case MOVIE:
+                long _id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+                if (_id > 0)
+                    returnUri = MovieContract.MovieEntry.buildMovieUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
     }
 
     @Override
@@ -95,5 +113,16 @@ public class MovieProvider  extends ContentProvider {
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         return 0;
+    }
+
+    // You do not need to call this method. This is a method specifically to assist the testing
+    // framework in running smoothly. You can read more at:
+    // http://developer.android.com/reference/android/content/ContentProvider.html#shutdown()
+    @Override
+    @TargetApi(11)
+    public void shutdown() {
+        mOpenHelper.close();
+        super.shutdown();
+
     }
 }
